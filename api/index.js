cd7ca4162de40e98c6ac90b709f51661528fb59c
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-08-12 00:43:59 +0000

    commit

--- api/index.js
+++ api/index.js
@@ -55,8 +55,8 @@ app.use((err, req, res, next) => {
 
 connectDB();
 
-app.listen(5000, () => {
-    console.log("Server connected successfully")
-})
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
